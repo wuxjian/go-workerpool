2a3e4fc588b8bf7b9f7e863bc7968354326dfb85
wuxjian <wu_xjian@163.com>
2020-11-09 11:51:36 +0800

    'finish'

--- dispatcher.go
+++ dispatcher.go
@@ -1,5 +1,33 @@
 package go_workerpool
 
-func Dispacher()  {
-	
+type Dispatcher struct {
+	WorkerPool chan chan Job
+	Len        int
+}
+
+func NewDispatcher(n int) *Dispatcher {
+	return &Dispatcher{
+		WorkerPool: make(chan chan Job, n),
+		Len:        n,
+	}
+}
+
+func (d *Dispatcher) Run() {
+	for i := 0; i < d.Len; i++ {
+		worker := NewWorker(d.WorkerPool)
+		worker.Start()
+	}
+
+}
+
+func (d *Dispatcher) dispatch() {
+	for {
+		select {
+		case job := <-JobChannel:
+			go func(job Job) {
+				jobChannel := <-d.WorkerPool
+				jobChannel <- job
+			}(job)
+		}
+	}
 }
--- worker.go
+++ worker.go
@@ -1,1 +1,48 @@
 package go_workerpool
+
+import "log"
+
+type Job interface {
+	Do() error
+}
+
+//job 队列
+var JobChannel = make(chan Job)
+
+type Worker struct {
+	WorkerPool chan chan Job
+	JobChannel chan Job
+	done       chan bool
+}
+
+func NewWorker(pool chan chan Job) *Worker {
+	return &Worker{
+		WorkerPool: pool,
+		JobChannel: make(chan Job),
+		done:       make(chan bool),
+	}
+}
+
+func (w *Worker) Start() {
+	go func() {
+		for {
+			//将自己的JobChannel注册到 WorkerPool中去
+			w.WorkerPool <- w.JobChannel
+			select {
+			case job := <-w.JobChannel:
+				err := job.Do()
+				if err != nil {
+					log.Printf("job do error:%v\n", err)
+				}
+			case <-w.done:
+				return
+			}
+		}
+	}()
+}
+
+func (w *Worker) Stop() {
+	go func() {
+		w.done <- true
+	}()
+}
